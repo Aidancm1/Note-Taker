237f3e0372f5d01219ecd9b407d7023680c29cf9
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-07-22 11:51:44 -0400

    Added some code to server.js

---
+++ public/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Server
+const PORT = 3001;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+// Static assets from public folder
+app.use(express.static('public'));
+
+// Get route for the home page
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+);
+
+
+// Get route for the notes page
+app.get('/feedback', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+);
+
+app.listen(PORT, () =>
+console.log('App listening at http://localhost:${PORT}')
+);

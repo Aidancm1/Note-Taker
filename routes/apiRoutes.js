674a3c914028749e5f49a970cda90e2fe5f289c3
Aidancm1 <aidancmorrow1@gmail.com>
2023-07-24 17:36:11 -0400

    added some better stuff

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+
+// GET route for notes
+router.get('/notes',(req,res)=>{
+    //logic for getting notes info
+});
+
+// POST route to update 
+router.post('/notes', (req,res)=>{
+    // logic for updating notes
+});
+
+// DELETE route to deleting note
+router.delete('/notes', (req,res)=>{
+    // logic for deleteing notes
+});
+
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const path = require("path");
+const fs = require("fs");
+
+// Get route for the notes page
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
+
+// Get route for the home page
+router.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+module.exports = router

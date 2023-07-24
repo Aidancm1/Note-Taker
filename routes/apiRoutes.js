f6ead6f7dfcd33a7bdd8f98b624067b03f7aa78d
Aidancm1 <aidancmorrow1@gmail.com>
2023-07-24 17:37:29 -0400

    forgot to save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
-
+const router = require("express").Router();
 
 // GET route for notes
-router.get('/notes',(req,res)=>{
-    //logic for getting notes info
+router.get("/notes", (req, res) => {
+  //logic for getting notes info
 });
 
-// POST route to update 
-router.post('/notes', (req,res)=>{
-    // logic for updating notes
+// POST route to update
+router.post("/notes", (req, res) => {
+  // logic for updating notes
 });
 
 // DELETE route to deleting note
-router.delete('/notes', (req,res)=>{
-    // logic for deleteing notes
+router.delete("/notes", (req, res) => {
+  // logic for deleteing notes
 });
 
+module.exports = router;
